354cf48e986babc8517806523344f5e9ca8a6d89
Juan Diego Collazos <jucollaas.gmail.com>
2025-05-20 15:47:30 -0500

    FLI improvement

--- factorizing-larget-integers/11476-3.cpp
+++ factorizing-larget-integers/11476-3.cpp
@@ -16,6 +16,8 @@ Primality Test | Set 3 (Miller–Rabin) : https://www.geeksforgeeks.org/primalit
 using namespace std;
 using ll = long long;
 
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
 ll gcd(ll a, ll b) {
   while (b != 0) {
     ll t = b;
@@ -26,89 +28,103 @@ ll gcd(ll a, ll b) {
 }
 
 ll mulmod(ll a, ll b, ll mod) {
-  ll result = 0;
+  ll ans = 0;
   a %= mod;
   while (b > 0) {
     if (b & 1)
-      result = (result + a) % mod;
+      ans = (ans + a) % mod;
     a = (a * 2) % mod;
     b >>= 1;
   }
-  return result;
+  return ans;
 }
 
 ll powmod(ll base, ll exp, ll mod) {
-  ll result = 1;
+  ll ans = 1;
   base %= mod;
   while (exp > 0) {
     if (exp & 1)
-      result = mulmod(result, base, mod);
+      ans = mulmod(ans, base, mod);
     base = mulmod(base, base, mod);
     exp >>= 1;
   }
-  return result;
+  return ans;
 }
 
-bool is_prime(ll n, int k = 5) {
-  if (n < 2) return false;
-  if (n == 2 || n == 3) return true;
-  if (n % 2 == 0) return false;
-
-  ll r = 0, d = n - 1;
-  while (d % 2 == 0) {
-    d /= 2;
-    r++;
-  }
-
-  mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-  for (int i = 0; i < k; ++i) {
-    ll a = rng() % (n - 3) + 2;
-    ll x = powmod(a, d, n);
-    if (x == 1 || x == n - 1)
-      continue;
-    bool composite = true;
-    for (ll j = 0; j < r - 1; ++j) {
-      x = powmod(x, 2, n);
-      if (x == n - 1) {
-        composite = false;
-        break;
-      }
+bool is_prime(ll n, int k = 5){
+  bool ans;
+  if (n < 2)
+    ans = false;
+  else if (n == 2 || n == 3)
+    ans = true;
+  else if (n % 2  == 0)
+    ans = false;
+  else{
+    ll r = 0, d = n - 1;
+    while (d % 2 == 0){
+      d >>= 1;
+      r++;
     }
-    if (composite)
-      return false;
+    ans = true;
+    int i = 0;
+    while(i < k && ans){
+      ll a = rng() % (n - 3) + 2;
+      ll x = powmod(a, d, n);
+      if (x != 1 && x != n -1){
+        ll j = 0;
+        bool composite = true;
+        while(j < r - 1 && composite){
+          x = powmod(x, 2, n);
+          if (x == n - 1)
+            composite = false;
+          ++j;
+        }
+        if (composite)
+          ans = false;
+      }
+      ++i;
+    }
   }
-  return true;
+  return ans;
+}
+
+ll f(ll x, ll n, ll c){
+  return (mulmod(x, x, n) + c) % n;
 }
 
 ll pollards_rho(ll n) {
-  if (n % 2 == 0) return 2;
-  if (is_prime(n)) return n;
-
-  mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-  while (true) {
-    ll c = rng() % n;
-    auto f = [&](ll x) { return (mulmod(x, x, n) + c) % n; };
-    ll x = 2, y = 2, d = 1;
-    while (d == 1) {
-      x = f(x);
-      y = f(f(y));
-      d = gcd(abs(x - y), n);
+  ll ans;
+  if (n % 2 == 0)
+    ans = 2;
+  else if(is_prime(n))
+    ans = n;
+  else{
+    ans = -1;
+    while(ans == -1){
+      ll c = rng() % n;
+      ll x = 2, y = 2, d = 1;
+      while(d == 1) {
+        x = f(x, n, c);
+        y = f(f(y, n, c), n, c);
+        d = gcd(abs(x - y), n);
+      }
+      if (d != n)
+        ans = d;
     }
-    if (d != n)
-      return d;
   }
+  return ans;
 }
 
-void factor(ll n, vector<ll>& factors) {
-  if (n == 1) return;
-  if (is_prime(n)) {
-    factors.push_back(n);
-    return;
+void factor(ll n, vector<ll>& factors){
+  if (n != 1){
+    if (is_prime(n)){
+      factors.push_back(n);
+    }else{
+      ll k = pollards_rho(n);
+      factor(k, factors);
+      factor(n / k, factors);
+    }
   }
-  ll f = pollards_rho(n);
-  factor(f, factors);
-  factor(n / f, factors);
 }
 
 int main() {
@@ -118,6 +134,7 @@ int main() {
     vector<ll> ans;
     factor(N, ans);
     sort(ans.begin(), ans.end());
+
     cout << N << " =";
     int act  = 0, n = ans.size();
     while(act < n){
